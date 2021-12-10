2affd93bfb0dff68f911476ac888ba330efc8ae9
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2021-12-10 16:42:05 +0100

    first commit

---
+++ TP1-LSP/src/IDuck.java
@@ -0,0 +1,15 @@
+
+public interface IDuck
+{
+    void quack() throws IDuckException;
+
+    void swim() throws IDuckException;
+
+    class IDuckException extends Exception
+    {
+        public IDuckException(String message)
+        {
+            super(message);
+        }
+    }
+}
